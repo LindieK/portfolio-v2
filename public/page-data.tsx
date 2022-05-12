b42d237248c5c272b426ea802fee7a1869441c5b
Linda Okorie <lindaokorie27@gmail.com>
2022-05-12 09:40:06 +0100

    fix: abstracted all sections text to a separate file
    This is so that addition to the sections can be easily done

---
+++ public/page-data.tsx
@@ -0,0 +1,38 @@
+export const HeroText = "Hey, I'm Linda Okorie. A Product Engineer combining my interest in design and code to build products that broaden perspectives about what is possible.";
+
+
+export const projects = [
+    {
+        title: "PG Photo Gallery",
+        description: "A Photo Gallery (with a totally uninspired name) that allows you to view, download, search for, and add photos to a personal collection",
+        tools: ["Firebase", "React", "Styled Components", "REST APIs"],
+        thumbnail: "/images/photo-gallery.jpg"
+    },
+    {
+        title: "Shortly URL Shortener",
+        description: "Shortly utilizes the shrtcode API to shorten URLs. The site saves past queries so you don’t have to repeat yourself if you lose the shortened URL.",
+        tools: ["React", "Sass", "REST APIs"],
+        thumbnail: "/images/url-shortener.jpg"
+    },
+    {
+        title: "Dispatcha",
+        description: "Dispatcha is a logistics application that is set to make package delivery seamless for people in Nigeria. On this projects I did idea validation, user research and product design.",
+        tools: ["Figma", "Miro"],
+        thumbnail: "images/Dispatcha.jpg"
+    }
+];
+
+export const Articles = [
+    {
+        title: "React Router’s useRoutes hook",
+        thumbnail: "images/ferenc-almasi-L8KQIPCODV8-unsplash"
+    },
+    {
+        title: "10 Icon packs from the Figma Community you should have in your toolkit",
+        thumbnail: "images/icon-pack"
+    },
+    {
+        title: "What Data Structures and Algorithims Mean To Me",
+        thumbnail: "images/tim-gouw-1K9T5YiZ2WU-unsplash"
+    }
+];
