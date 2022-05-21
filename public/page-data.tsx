6f4b309c0b30fe3d7eb4faeae8bb021cfdff7c75
Linda Okorie <lindaokorie27@gmail.com>
2022-05-21 22:47:57 +0100

    fix: added image file extensions and links to porjects

--- public/page-data.tsx
+++ public/page-data.tsx
@@ -6,33 +6,36 @@ export const projects = [
         title: "PG Photo Gallery",
         description: "A Photo Gallery (with a totally uninspired name) that allows you to view, download, search for, and add photos to a personal collection",
         tools: ["Firebase", "React", "Styled Components", "REST APIs"],
-        thumbnail: "/images/photo-gallery.jpg"
+        thumbnail: "/images/photo-gallery.jpg",
+        link: "https://linda-photo-gallery.web.app"
     },
     {
         title: "Shortly URL Shortener",
         description: "Shortly utilizes the shrtcode API to shorten URLs. The site saves past queries so you don’t have to repeat yourself if you lose the shortened URL.",
         tools: ["React", "Sass", "REST APIs"],
-        thumbnail: "/images/url-shortener.jpg"
+        thumbnail: "/images/url-shortener.jpg",
+        link: "https://lindiek.github.io/url-shortner"
     },
     {
         title: "Dispatcha",
         description: "Dispatcha is a logistics application that is set to make package delivery seamless for people in Nigeria. On this projects I did idea validation, user research and product design.",
         tools: ["Figma", "Miro"],
-        thumbnail: "images/Dispatcha.jpg"
+        thumbnail: "/images/Dispatcha.jpg",
+        link: "https://dribbble.com/shots/17373512-Dispatcha-Delivery-App?utm_source=Clipboard_Shot&utm_campaign=lindaokorie&utm_content=Dispatcha%20-%20Delivery%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=lindaokorie&utm_content=Dispatcha%20-%20Delivery%20App&utm_medium=Social_Share"
     }
 ];
 
 export const Articles = [
     {
         title: "React Router’s useRoutes hook",
-        thumbnail: "images/ferenc-almasi-L8KQIPCODV8-unsplash"
+        thumbnail: "/images/ferenc-almasi-L8KQIPCODV8-unsplash.jpg"
     },
     {
         title: "10 Icon packs from the Figma Community you should have in your toolkit",
-        thumbnail: "images/icon-pack"
+        thumbnail: "/images/icon-pack.jpg"
     },
     {
         title: "What Data Structures and Algorithims Mean To Me",
-        thumbnail: "images/tim-gouw-1K9T5YiZ2WU-unsplash"
+        thumbnail: "/images/tim-gouw-1K9T5YiZ2WU-unsplash.jpg"
     }
 ];
