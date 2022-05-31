059dd9d381e9901cecc09427bf25aeb7356b9eae
Linda Okorie <lindaokorie27@gmail.com>
2022-05-31 10:14:27 +0100

    fix: corrected delay of animations

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react'
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React from 'react'
 
 import styles from "./Projects.module.scss"
 import { projects } from '../../public/page-data';
 import Project from '../project/Project';
 
-const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] }
-const projectVariants = {
-  initial: { scale: 0.9, opacity: 0},
-  enter: { scale: 1, opacity: 1, transition }
-}
-
 
 const Projects = () => {
-  const control = useAnimation();
-  const [ ref, inView ] = useInView();
-  
-  useEffect(()=> {
-    if(inView){
-      control.start("enter");
-    }
-  }, [control, inView]);
 
   return (
     <section id="projects" className={styles.projects}>
@@ -30,13 +14,13 @@ const Projects = () => {
           <h2>Projects</h2>
           <p>I am always experimenting with new technologies and improving my knowledge in product design and development</p>
         </div>
-        <motion.div ref={ref} initial="initial" animate={control} variants={ { enter: { transition: { delay: 1, staggerChildren: 0.25 } } } }>
+        <div>
           {projects.map((item, index) => {
             return(
-              <Project key={index} thumbnail={item.thumbnail} title={item.title} desc={item.description} tools={item.tools} link={item.link} projectVariant={projectVariants}/>
+              <Project key={index} thumbnail={item.thumbnail} title={item.title} desc={item.description} tools={item.tools} link={item.link} />
             )
           })}
-        </motion.div>
+        </div>
       </div>
     </section>
   )
