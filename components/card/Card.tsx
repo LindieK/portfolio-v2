3b3bd717b2d6104e788a8997789eb05d7f93a8bf
Linda Okorie <lindaokorie27@gmail.com>
2022-06-04 13:30:51 +0100

    fix: attempt at fixing bug
    The articles do not how on mobile even though the intersection observer was clearly working. They also appear too small on some tablets.

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image"
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
 
 import styles from './Card.module.scss'
 import { useCursorContext } from '../../context/CursorContext'
@@ -9,15 +10,29 @@ interface Card {
   thumbnail: string;
   title: string;
   link: string;
-  cardVariant: any;
 }
-const Card = ({thumbnail, title, link, cardVariant}: Card) => {
+
+const transition = { duration: 1.8, ease: [0.43, 0.13, 0.23, 0.96] }
+const cardVariant = {
+  initial: { scale: 0.9, opacity: 0},
+  enter: { scale: 1, opacity: 1, transition }
+}
+
+const Card = ({thumbnail, title, link}: Card) => {
   const { articleEnter, articleLeave } = useCursorContext();
+  const cardControl = useAnimation();
+  const [ ref, inView ] = useInView();
+
+  useEffect(() => {
+    if(inView){
+      cardControl.start("enter");
+    }
+  },[cardControl, inView])
   
   return (
-    <motion.div className={styles.cardContainer} variants={cardVariant} onMouseEnter={articleEnter} onMouseLeave={articleLeave}>
+    <motion.div className={styles.cardContainer} ref={ref} variants={cardVariant} initial="initial" animate={cardControl} onMouseEnter={articleEnter} onMouseLeave={articleLeave}>
       <a href={link} className={styles.link} target="_blank" rel="noreferrer">
-        <Image src={thumbnail} width={351} height={280} alt="article thumbnail"/>
+        <Image src={thumbnail} width={1300} height={1300} layout="responsive" alt="article thumbnail"/>
         <h3>{title}</h3>
       </a>
     </motion.div>
